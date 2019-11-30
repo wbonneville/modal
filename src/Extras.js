1bce48db1b85ad2ea5c9959241bbf56bfdf116ec
Wesley Bonneville <westazbonneville@gmail.com>
2019-11-30 12:02:02 -0500

    Update Extras.js

--- src/Extras.js
+++ src/Extras.js
@@ -561,3 +561,21 @@ console.log(
     3
   )
 );
+
+let myNestedArray = [
+  // change code below this line
+  [
+    [
+      "deep",
+      [
+        "deeper",
+        ["unshift", "deepest", false, 1, 2, 3, "complex", "nested"],
+        ["loop", "shift", 6, 7, 1000, "method"],
+        ["concat", false, true, "spread", "array"],
+        ["mutate", 1327.98, "splice", "slice", "push"],
+        ["iterate", 1.3849, 7, "8.4876", "arbitrary", "depth"]
+        // change code above this line
+      ]
+    ]
+  ]
+];
