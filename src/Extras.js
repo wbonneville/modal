2c184e749003ce2454811550a668871091687cac
Wesley Bonneville <westazbonneville@gmail.com>
2019-11-30 11:15:23 -0500

    Update Extras.js

--- src/Extras.js
+++ src/Extras.js
@@ -523,3 +523,16 @@ function countdown(myArray, n) {
     countdown(myArray, n - 1);
   }
 }
+
+function quickCheck(arr, elem) {
+  // change code below this line
+  if (arr.indexOf(elem) >= 0) {
+    return true;
+  } else {
+    return false;
+  }
+  // change code above this line
+}
+
+// change code here to test different cases:
+console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
