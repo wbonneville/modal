87837b5923d9ab766b87c2708894d937f5996664
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-01 17:42:55 -0500

    Update Extras.js

--- src/Extras.js
+++ src/Extras.js
@@ -679,3 +679,56 @@ function countOnline(usersObj) {
 
   // change code above this line
 }
+
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  // change code below this line
+  return Object.keys(obj);
+  // change code above this line
+}
+
+console.log(getArrayOfUsers(users));
+
+let user = {
+  name: "Kenneth",
+  age: 28,
+  data: {
+    username: "kennethCodesAllDay",
+    joinDate: "March 26, 2016",
+    organization: "freeCodeCamp",
+    friends: ["Sam", "Kira", "Tomo"],
+    location: {
+      city: "San Francisco",
+      state: "CA",
+      country: "USA"
+    }
+  }
+};
+
+function addFriend(userObj, friend) {
+  // change code below this line
+  let friends = userObj.data.friends;
+  friends[friends.length] = friend;
+  return friends;
+  // change code above this line
+}
+
+console.log(addFriend(user, "Pete"));
