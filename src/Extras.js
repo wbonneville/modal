88e58a7903f145d87c8e5403cd5119e2984f4b9e
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-02 10:26:51 -0500

    Update Extras.js

--- src/Extras.js
+++ src/Extras.js
@@ -732,3 +732,17 @@ function addFriend(userObj, friend) {
 }
 
 console.log(addFriend(user, "Pete"));
+
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+// Add your code below this line
+for (let prop in canary) {
+  if (canary.hasOwnProperty(prop)) {
+    ownProps.push(prop);
+  }
+}
