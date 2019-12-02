6d589775da830a8f22fff8f41dd50f286ba3b038
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-02 11:09:57 -0500

    Update Extras.js

--- src/Extras.js
+++ src/Extras.js
@@ -776,3 +776,13 @@ for (let prop in beagle) {
     prototypeProps.push(prop);
   }
 }
+
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+// Add your code below this line
+
+Dog.prototype.isPrototypeOf(beagle);
