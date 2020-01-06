2c8d00cd4714529f73e3cbf0db706810f026caca
Wesley Bonneville <westazbonneville@gmail.com>
2020-01-06 12:46:44 -0500

    Update Extras.js

--- src/Extras.js
+++ src/Extras.js
@@ -994,6 +994,7 @@ var s = [23, 65, 98, 5];
 Array.prototype.myFilter = function(callback) {
   var newArray = [];
   // Add your code below this line
+  // x = item
   this.forEach(function(x) {
     if (callback(x) == true) {
       newArray.push(x);
