f5ff9d35ca41e2b78ed93743b5ca140021076e6b
Wesley Bonneville <westazbonneville@gmail.com>
2020-01-14 14:54:41 -0500

    Update Extras.js

--- src/Extras.js
+++ src/Extras.js
@@ -1491,3 +1491,29 @@ whatIsInAName(
   [{ apple: 1, bat: 2 }, { bat: 2 }, { apple: 1, bat: 2, cookie: 2 }],
   { apple: 1, bat: 2 }
 );
+
+// working solution...
+
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+
+  var other = Object.keys(source);
+  arr = collection.filter(val =>
+    other.every(item => source[item] === val[item])
+  );
+
+  console.log(arr);
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName(
+  [
+    { first: "Romeo", last: "Montague" },
+    { first: "Mercutio", last: null },
+    { first: "Tybalt", last: "Capulet" }
+  ],
+  { last: "Capulet" }
+);
